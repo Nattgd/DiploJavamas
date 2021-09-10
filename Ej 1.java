46b397a3accaa2fccef336ebb937a17b07f78bec
Nattgd <90103744+Nattgd@users.noreply.github.com>
2021-09-10 17:02:05 -0300

    Create Ej 1.java
    //Me piden ingresados las medidas de un terreno X=frente, Y=fondo, calcular y mostrar la cantidad de metros de alambre para cercarlo, es decir su perímetro.

---
+++ Ej 1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Ej1 {
+    public static void main(String[] args) {
+      Scanner entrada=new Scanner(System.in);
+      int x;
+      int y;
+      System.out.println("ingrese el largo y ancho del terreno");
+      x=entrada.nextInt();
+      y=entrada.nextInt();
+      z=x*2+y*2;
+      System.out.println("Se necesitará "+z+" mts de alambre");
+    }
+}
+      
